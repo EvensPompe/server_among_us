5f8ae9ddfaf05e758597d3b3845b96ecb46d9b00
Evens Pompe <evenspompe9593@gmail.com>
2022-07-01 10:11:51 +0200

    fix(SocketServer): fix addNewPlayer

--- classes/SocketServer.js
+++ classes/SocketServer.js
@@ -1,25 +1,41 @@
-const Player = require('./Player');
+const Player = require("./Player");
 
 module.exports = class SocketServer {
-    constructor(server) {
-        this._io = require('socket.io')(server, {
-            cors: {
-                origin: "*",
-                methods: ["GET", "POST"]
-            }
-        });
-        this._io.on('connection', (client) => {
-            this.connection(client);
-        });
-        this.players = [];
-    }
+  constructor(server) {
+    this._io = require("socket.io")(server, {
+      cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+      },
+    });
+    this._io.on("connection", (socket) => this.connect(socket));
+    this.players = [];
+  }
 
-    connection(client) {
-        this.addNewPlayer(client)
-    }
+  connect(socket) {
+    socket.on("client:user:new", (client) => this.addNewPlayer(socket, client));
+  }
 
-    addNewPlayer(client) {
-        const newPlayer = new Player("Moi", client.id);
-        this._io.emit("newPlayer", { name: newPlayer._name, id: newPlayer._id });
+  addNewPlayer(socket, client) {
+    const alreadyExists = this.players.some(
+      (player) => player._name === client.name
+    );
+    if (!alreadyExists) {
+      const newPlayer = new Player(client.name, client.id);
+      console.log(
+        "New Player ! name: %s, id:%s",
+        newPlayer._name,
+        newPlayer._id
+      );
+      this.players.push(newPlayer);
+      console.log(this.players);
+      this._io.emit("server:player:new", {
+        name: newPlayer._name,
+        id: newPlayer._id,
+      });
+      socket.emit("server:auth:login", { success: true, message: "Connexion effectuée avec succès" });
+    } else {
+      socket.emit("server:auth:login", { success: false, message: "Ce nom d'utilisation est déjà utilisé" });
     }
-}
+  }
+};
